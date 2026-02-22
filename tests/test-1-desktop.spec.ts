ea897daa81ff834757f22030eec5697ed3df51e9
Svitlana Amelchenko <svetylya6m@gmail.com>
2026-02-22 18:13:02 +0200

    Add Products text visibility test

--- tests/test-1-desktop.spec.ts
+++ tests/test-1-desktop.spec.ts
@@ -5,4 +5,5 @@ test('test-descktop-viewport', async ({ page }) => {
   await page.locator('[data-test="username"]').fill('standard_user');
   await page.locator('[data-test="password"]').fill('secret_sauce');
   await page.locator('[data-test="login-button"]').click();
+  await expect(page.getByText("Products")).toBeVisible();
 });
